06b202faa2421be003eb0b01634dcd4e5f32379e
saeki4n <saeki.oshigoto@gmail.com>
2025-05-09 18:34:14 +0900

    Markup memo list screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,57 @@
 import { JSX } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const Index = (): JSX.Element => {
   return (
-    <View>
+    <View style={styles.container}>
       <View>
         <View>
           <Text>Memp App</Text>
           <Text>ログアウト</Text>
         </View>
       </View>
+      <View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023年10月1日</Text>
+          </View>
+          <View>
+            <Text>ｘ</Text>
+          </View>
+        </View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023年10月1日</Text>
+          </View>
+          <View>
+            <Text>ｘ</Text>
+          </View>
+        </View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023年10月1日</Text>
+          </View>
+          <View>
+            <Text>ｘ</Text>
+          </View>
+        </View>
+      </View>
+      <View>
+        <Text>+</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
 
 export default Index;
